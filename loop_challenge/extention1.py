c4dae59261f83042d3fb52f6b795587ddca04bd9
Kiran Wood <kiranalexander21@gmail.com>
2025-12-12 17:05:34 +0000

    Completed basic starter files for loop challenge tasks

--- loop_challenge/extention1.py
+++ loop_challenge/extention1.py
@@ -3,29 +3,34 @@
 # global variables
 scorelist = {}
 score_info = {}
+top5 = []
 
+# Function to take input of the scores
 def score_input():
     ## Take an input of scores and names
     ## Keep names and scores linked    
     return scorelist
 
+# Function to compute the score information
 def compute_scores(scorelist):
     ## Total Scores Entered
     ## Average Score
     ## Top 5 Scores
+        # No. of people with Highest Score
+    # Dictionary of info
     return score_info
 
-def print_scores():
+# Function to print the scores
+def print_scores(score_info):
     ## Display the:
     # Total Scores Entered
     # Average Score
-    # Top 5 scores and
-    #   The names attached to them
+    # Top 5 scores and names attached to them
     return
 
 def main():
-    scorelist = score_input()
-    score_info = compute_scores(scorelist)
-    print_scores(score_info)
+    # Combines all the functions together
+    print_scores(compute_scores(score_input()))
 
+## Runs the program
 main()
--- loop_challenge/main_task.py
+++ loop_challenge/main_task.py
@@ -4,28 +4,28 @@
 scorelist = []
 score_info = {}
 
+# Function to take input of the scores
 def score_input():
-    ## Take an input of scores.
-    ## Create list of scores to work with
+    # Take an input of scores.
+    # Split single input into list (named scorelist) of numbers
     return scorelist
 
+# Function to compute the score information
 def compute_scores(scorelist):
     ## Total Scores Entered
     ## Average Score
     ## Highest Score
     ## No. of people with Highest Score
+    # Dictionary of info called score_info
     return score_info
 
+# Function to print the scores
 def print_scores(score_info):
-    ## Display:
-    # Total Scores Entered
-    # Average Score
-    # Highest Score
-    # No. of people with Highest Score
-    return
+    ## Display the dictionary score_info in an appropriate way
 
 def main():
-    ## run your functions here
-    return
+    ## combines your functions as one here
+    print_scores(compute_scores(score_input()))
 
+# runs the program
 main()
