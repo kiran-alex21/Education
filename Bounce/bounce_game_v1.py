4401ffd17e3b889ec076b8d75dd2f6eb420b5a67
Kiran <kiranalexander21@gmail.com>
2026-02-15 17:20:34 +0000

    Updates to Bounce
    Added Game Over text, and a score counter.

--- Bounce/bounce_game_v1.py
+++ Bounce/bounce_game_v1.py
@@ -6,7 +6,7 @@ import time
 class Ball:
     # initiation function setup
     def __init__(self, canvas, paddle, color):
-        self.canvas = canvas # get a variavle for the canvas
+        self.canvas = canvas # get a variable for the canvas
         self.paddle = paddle # create a paddle variable in ball
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color) # get a variable for the ball
         self.canvas.move(self.id, 245, 100) # move the ball
@@ -16,7 +16,7 @@ class Ball:
         self.y = -3
         self.canvas_height = self.canvas.winfo_height() # variable for current window height
         self.canvas_width = self.canvas.winfo_width() # variable for current window width
-        self.hit_bottom = False # variable for hitting bottom
+        self.hit_bottom = False # variable for hitting bottom    
     
     # detect if ball and paddle touch
     def hit_paddle(self, pos):
@@ -37,12 +37,14 @@ class Ball:
             self.hit_bottom = True
         # if the ball hits the paddle, change y
         if self.hit_paddle(pos) == True:
-            self.y = -3
+            self.y = -3 - (abs(paddle.x)/2)
         # if ball at sides, change x direction
         if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
+      
+
 
 # Paddle Class
 class Paddle:
@@ -73,28 +75,93 @@ class Paddle:
     def turnright(self, evt):
         self.x = 3
 
-# Window Setup
-tk = Tk() # create a window
-tk.title("Paddle Bounce") # name the window
-tk.resizable(0,0) # stop resizing
+# Score Class
+class Score:
+    def __init__(self, canvas, score):
+        self.canvas = canvas
+        self.score = score
+        self.display = self.canvas.create_text(0, 0, text=str(self.score), font=('AtkinsonHyperlegible', '12'))
+        self.canvas.move(self.display, 10, 10) # move text
 
-# Create background
-canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0, background='blue') # create a canvas
-# put the canvas into the window
-canvas.pack()
-tk.update()
+    def updateScore(self):
+        pos = ball.canvas.coords(ball.id) # variable for current position of ball
+        if ball.hit_paddle(pos) == True:
+            self.score = self.score + 1
+        self.canvas.itemconfig(self.display, text=str(self.score))
 
+
+# Game Over Class
+class GameOver:
+    def __init__(self, canvas):
+        self.canvas = canvas # set variable for canvas
+        self.text = self.canvas.create_text(0, 0, text="GAME OVER!", font=('AtkinsonHyperlegible', '15')) # create game over text.
+        self.canvas.move(self.text, 250, 200) # move text to centre
+        self.canvas.itemconfig(self.text, state=HIDDEN) # hide text
+    
+    def endgame(self, isOver):
+        while isOver == False:
+            self.canvas.itemconfig(self.text, state=HIDDEN)  # hide text while game is playing
+        else:
+            time.sleep(0.5) # sleep for 1/2 a second
+            self.canvas.itemconfig(self.text, state=NORMAL) # show game over text
+
+# Game Start Class
+class GameStart:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.playing = False # variable for if the game has started
+        self.canvas.bind_all('<Return>', self.startgame) # bind startgame function to the enter key
+    
+    # function to start the game
+    def startgame(self, evt):
+         self.playing = True
+
+# Window class
+class Window:
+    def startup(self):
+        # Window Setup
+        self.tk = Tk() # create a window
+        self.tk.title("Paddle Bounce") # name the window
+        self.tk.resizable(0,0) # stop resizing
+        # Create background
+        self.canvas = Canvas(self.tk, width=500, height=400, bd=0, highlightthickness=0, background='blue') # create a canvas
+        # put the canvas into the window
+        self.canvas.pack()
+        self.tk.update()
+
+    # Update entire window
+    def update_window(self):
+        self.tk.update_idletasks() # update background tasks
+        self.tk.update() # update the window
+
+# Create window
+window = Window()
+window.startup()
+
+# Create Game Start
+gameStart = GameStart(window.canvas)
 # Create a Paddle
-paddle = Paddle(canvas, 'purple')
+paddle = Paddle(window.canvas, 'purple')
 # Create a ball
-ball = Ball(canvas, paddle, 'red')
+ball = Ball(window.canvas, paddle, 'red')
+#Create Score
+score = Score(window.canvas, 0)
+# Create Game Over
+gameEnd = GameOver(window.canvas)
+
 
 # Keeps window open + constantly refeshing
 while 1:
-    # game lossing mechanic
-    if ball.hit_bottom == False:
-        ball.draw() # move ball
+    while gameStart.playing == False:
+        window.update_window() # Update entire window
+        time.sleep(0.01) # sleep for 1/100 of a second
+    score.updateScore()
+    while ball.hit_bottom == False:
         paddle.draw() # allows paddle movement updates
-    tk.update_idletasks() # update background tasks
-    tk.update() # update the window
-    time.sleep(0.01) # sleep for 2/100 of a second
+        ball.draw() # move ball
+        score.updateScore() # Update Score
+        window.update_window() # Update entire window
+        time.sleep(0.01) # pause for 1/1000 of a second
+    gameEnd.endgame(ball.hit_bottom) # Checks Game Ending
+    window.update_window() # Update entire window
+    time.sleep(0.01) # sleep for 1/100 of a second
