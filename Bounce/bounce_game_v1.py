2d6ae3b084fdb872d4970ecccf9e2965bfc91403
Kiran <kiranalexander21@gmail.com>
2026-03-10 22:50:52 +0000

    Enhance game with high score and replay features
    Added high score tracking and replay functionality.

--- Bounce/bounce_game_v1.py
+++ Bounce/bounce_game_v1.py
@@ -80,7 +80,8 @@ class Score:
     def __init__(self, canvas, score):
         self.canvas = canvas
         self.score = score
-        self.display = self.canvas.create_text(0, 0, text=str(self.score), font=('AtkinsonHyperlegible', '12'))
+        self.highscore = 0
+        self.display = self.canvas.create_text(0, 0, fill = "black", text=str(self.score), font=('AtkinsonHyperlegible', '12'))
         self.canvas.move(self.display, 10, 10) # move text
 
     def updateScore(self):
@@ -88,22 +89,62 @@ class Score:
         if ball.hit_paddle(pos) == True:
             self.score = self.score + 1
         self.canvas.itemconfig(self.display, text=str(self.score))
+        return self.score
+
+    def highScore(self, newScore):
+        self.newScore = newScore
+        if self.newScore > self.highscore:
+            self.highscore = self.newScore
+        return self.highscore
 
 
 # Game Over Class
 class GameOver:
-    def __init__(self, canvas):
+    def __init__(self, canvas, ball, score):
         self.canvas = canvas # set variable for canvas
-        self.text = self.canvas.create_text(0, 0, text="GAME OVER!", font=('AtkinsonHyperlegible', '15')) # create game over text.
+        self.ball = ball
+        self.score = score
+        self.text = self.canvas.create_text(0, 0, text="GAME OVER!", fill="black", font=('AtkinsonHyperlegible', '15')) # create game over text.
+        self.scoretext = self.canvas.create_text(0, 0, font=('AtkinsonHyperlegible', '15'))
+        self.rplybtn = self.canvas.create_rectangle(200, 230, 300, 270, fill="red", tags="replay_button")
+        self.rplytext = self.canvas.create_text(250, 250, text="Replay", fill="black",  font=('AtkinsonHyperlegible', '15'), tags="replay_button")
         self.canvas.move(self.text, 250, 200) # move text to centre
+        self.canvas.move(self.scoretext, 250, 100)
         self.canvas.itemconfig(self.text, state=HIDDEN) # hide text
+        self.canvas.itemconfig(self.scoretext, state=HIDDEN)
+        self.canvas.itemconfig("replay_button", state=HIDDEN)
+        self.canvas.tag_bind("replay_button", "<Button-1>", self.replay)
     
     def endgame(self, isOver):
         while isOver == False:
             self.canvas.itemconfig(self.text, state=HIDDEN)  # hide text while game is playing
+            self.canvas.itemconfig("replay_button", state=HIDDEN)
+            self.canvas.itemconfig(self.scoretext, state=HIDDEN)         
         else:
             time.sleep(0.5) # sleep for 1/2 a second
-            self.canvas.itemconfig(self.text, state=NORMAL) # show game over text
+            self.canvas.itemconfig(self.text, state=NORMAL)
+            self.canvas.itemconfig("replay_button", state=NORMAL)
+            self.canvas.itemconfig(self.scoretext, state=NORMAL)
+            final_score = self.score.score
+            high_score = self.score.highScore(final_score)
+            text = ("Your Score is " + str(final_score) + "\nYour Highest Score is " + str(high_score))
+            self.canvas.itemconfig(self.scoretext, text=text)
+
+    def replay(self, event):
+        # Hide UI
+        self.canvas.itemconfigure(self.text, state="hidden")
+        self.canvas.itemconfigure(self.scoretext, state="hidden")
+        self.canvas.itemconfigure("replay_button", state="hidden")
+        # Reset game variables
+        self.ball.hit_bottom = False
+        # Reset ball
+        self.canvas.coords(self.ball.id, 243, 85, 258, 100)
+        starts = [-3, -2, 1, 1, 2, 3] # list of possible x starts
+        random.shuffle(starts)
+        self.ball.x = starts[0]
+        self.ball.y = -3
+        # Reset score
+        self.score.score = 0
 
 # Game Start Class
 class GameStart:
@@ -147,10 +188,10 @@ ball = Ball(window.canvas, paddle, 'red')
 #Create Score
 score = Score(window.canvas, 0)
 # Create Game Over
-gameEnd = GameOver(window.canvas)
+gameEnd = GameOver(window.canvas, ball, score)
 
 
-# Keeps window open + constantly refeshing
+# Keeps window open + constantly refreshing
 while 1:
     while gameStart.playing == False:
         window.update_window() # Update entire window
